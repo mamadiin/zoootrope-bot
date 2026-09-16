fc4a3828a0c3cc0d3ec0879140f474d860307d05
mamadiin <mohamadaminshabani1377@gmail.com>
2026-09-17 00:21:01 +0330

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ if not GEMINI_API_KEY:
 genai.configure(api_key=GEMINI_API_KEY)
 
 # مدل پایدار و مطمئن
-MODEL_NAME = "gemini-1.5-flash-latest"
+MODEL_NAME = "gemini-pro"
 SIGNATURE = "@zoootrope"
 MAX_MESSAGE_LENGTH = 4000
 
